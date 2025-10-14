76e08a244c755a690b8a21eccd860b74b835171c
laclac102 <phanvothungabiotech@gmail.com>
2025-10-14 16:05:25 +1100

    test: update authentication tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
 
   beforeEach(async () => {
+    const mockAuthService = {
+      loginWithRedirect: () => {},
+      isAuthenticated$: of(false),
+      user$: of(null),
+      getAccessTokenSilently: () => of(undefined)
+    } as unknown as AuthService;
+
     await TestBed.configureTestingModule({
-      imports: [AppComponent]
+      imports: [AppComponent],
+      providers: [{ provide: AuthService, useValue: mockAuthService }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
