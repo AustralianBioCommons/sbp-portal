1f5aa9fd73ed359ef7c56d2659ae74b51f0caa2e
laclac102 <phanvothungabiotech@gmail.com>
2025-10-14 15:52:44 +1100

    chore: update Auth0 client ID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   auth: {
     domain: 'dev.login.aai.test.biocommons.org.au',
-    clientId: 'oXjPRRuMIVrzpJY7Ku2ojs0gZNmPVKmS',
+    clientId: 'VgTSGK8Ph92r8mVhmVvQDrxGzbWX0vCm',
   }
 };
