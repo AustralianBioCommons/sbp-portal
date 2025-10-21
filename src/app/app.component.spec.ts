26ad7f67a12725a22f2565051d17c884f0661870
laclac102 <phanvothungabiotech@gmail.com>
2025-10-22 10:07:41 +1100

    test: improve test coverage and reliability

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -47,7 +47,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     const button = compiled.querySelector('button');
-    expect(button?.textContent).toContain('Log in');
+    expect(button?.textContent).toContain('Sign up or log in');
     button?.click();
     expect(mockAuthService.login).toHaveBeenCalled();
   });
