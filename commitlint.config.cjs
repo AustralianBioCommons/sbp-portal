b7b7bc474c7e10ca44d9949b55955c13511518a2
laclac102 <phanvothungabiotech@gmail.com>
2025-10-22 15:46:48 +1100

    build: resolve commitlint configuration conflicts

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Enforce a fixed set of allowed commit types (Conventional Commits)
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']],
-    // Keep subject case enforcement disabled (no casing rule).
-    'subject-case': [0, 'always'],
-  },
+  extends: ['@commitlint/config-conventional']
 };
