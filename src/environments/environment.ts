54fe78bbab0e9055bfba3cdd29870c326e1b9818
laclac102 <phanvothungabiotech@gmail.com>
2025-12-18 13:34:09 +1100

    refactor: update backend API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,6 +16,6 @@ export const environment: Environment = {
     clientId: "VgTSGK8Ph92r8mVhmVvQDrxGzbWX0vCm",
     audience: "https://dev.api.aai.test.biocommons.org.au",
   },
-  apiBaseUrl: "http://localhost:3000",
+  apiBaseUrl: "https://api.dev.sbp.test.biocommons.org.au",
   profileUrl: "https://dev.portal.aai.test.biocommons.org.au/profile",
 };
