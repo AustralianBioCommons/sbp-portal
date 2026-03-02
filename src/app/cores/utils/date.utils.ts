70fa2517eef28b0467a5a3ee5d0a0821adfc26ec
laclac102 <phanvothungabiotech@gmail.com>
2026-03-03 09:27:29 +1100

    refactor: add date-fns, refctor apiUrl

--- src/app/cores/utils/date.utils.ts
+++ src/app/cores/utils/date.utils.ts
@@ -1,13 +1,5 @@
-export function formatDateTimeForJobs(dateString: string): string {
-  const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
-  const hours24 = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const period = hours24 >= 12 ? "PM" : "AM";
-  const hours12 = hours24 % 12 || 12;
-  const hours = String(hours12).padStart(2, "0");
+import { format } from "date-fns";
 
-  return `${day}-${month}-${year} ${hours}:${minutes}${period}`;
+export function formatDateTimeForJobs(dateString: string): string {
+  return format(new Date(dateString), "dd-MM-yyyy hh:mmaaa");
 }
