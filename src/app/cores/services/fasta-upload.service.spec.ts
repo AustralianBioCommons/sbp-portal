62d727ff2f0ab2175c7d3b278b319c33a23c6f94
laclac102 <phanvothungabiotech@gmail.com>
2026-04-30 12:29:45 +1000

    fix: lint

--- src/app/cores/services/fasta-upload.service.spec.ts
+++ src/app/cores/services/fasta-upload.service.spec.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from "@angular/common/http";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 import { FastaUploadService } from "./fasta-upload.service";
