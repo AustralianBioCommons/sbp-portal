e9baeb7e288768b9919178b5b963daeb4ac596c4
amandazhu <amandazhuyilan@gmail.com>
2026-05-22 18:39:46 +1000

    fix: remove default values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,14 +7,14 @@ import {
 const defaults: Environment = {
   production: true,
   auth: {
-    domain: "dev.login.aai.test.biocommons.org.au",
-    clientId: "VgTSGK8Ph92r8mVhmVvQDrxGzbWX0vCm",
-    audience: "https://dev.api.aai.test.biocommons.org.au",
+    domain: "",
+    clientId: "",
+    audience: "",
   },
-  apiBaseUrl: "https://api.dev.sbp.test.biocommons.org.au",
-  profileUrl: "https://dev.portal.aai.test.biocommons.org.au/profile",
-  rolesClaim: "https://biocommons.org.au/roles",
-  workflowRole: "biocommons/group/sbp_workflow_execution",
+  apiBaseUrl: "",
+  profileUrl: "",
+  rolesClaim: "",
+  workflowRole: "",
 };
 
 export const environment: Environment = mergeEnvironmentConfig(defaults);
